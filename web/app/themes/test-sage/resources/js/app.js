6f3acc50fe39a5fd46e0dca48f5d01dcbf37eecb
Christos Papoulas <papoulas@hellenictechnologies.com>
2025-04-17 10:52:28 +0300

    Basic page functionality

---
+++ web/app/themes/test-sage/resources/js/app.js
@@ -0,0 +1,12 @@
+import.meta.glob([
+  '../images/**',
+  '../fonts/**',
+]);
+
+// Our modules / classes
+import MobileMenu from "./modules/MobileMenu"
+import HeroSlider from "./modules/HeroSlider"
+
+// Instantiate a new object using our modules/classes
+var mobileMenu = new MobileMenu()
+var heroSlider = new HeroSlider()
---
+++ web/app/themes/test-sage/resources/views/partials/page-header.blade.php
@@ -0,0 +1,11 @@
+<div class="page-header">
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url('{{ Vite::asset('resources/images/ocean.jpg') }}');"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title">{!! $title !!}</h1>
+      <div class="page-banner__intro">
+        <p>DON'T FORGET TO REPLACE ME LATER</p>
+      </div>
+    </div>
+  </div>
+</div>
