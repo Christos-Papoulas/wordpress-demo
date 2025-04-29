790d4e7cb8745ff23301e4d34086a5ca43e750b8
Christos Papoulas <papoulas@hellenictechnologies.com>
2025-04-29 15:59:50 +0300

    Add search for custom post types

--- web/app/themes/test-sage/app/Http/Controllers/SearchController.php
+++ web/app/themes/test-sage/app/Http/Controllers/SearchController.php
@@ -7,21 +7,29 @@ use WP_Query;
 
 class SearchController
 {
-    public function index(Request $request)
+    public function index(Request $request): array
     {
-        $professors = new WP_Query([
-           'post_type' => 'professor',
+        $mainQuery = new WP_Query([
+           'post_type' => ['post', 'page', 'professor', 'program', 'campus', 'event'],
            's' => sanitize_text_field($request->input('term')),
         ]);
 
-        $results = [];
-        while ($professors->have_posts()) {
-            $professors->the_post();
-            $results[] = [
+        $results = [
+            'post' => [],
+            'page' => [],
+            'professor' => [],
+            'program' => [],
+            'campus' => [],
+            'event' => [],
+        ];
+        while ($mainQuery->have_posts()) {
+            $mainQuery->the_post();
+            $results[get_post_type()][] = [
                 'title' => get_the_title(),
                 'permalink' => get_the_permalink(),
             ];
         }
+
         return $results;
     }
 }
